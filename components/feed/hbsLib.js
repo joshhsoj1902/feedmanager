1fd51c78f44b78469b4a70b2a9f2a5194c6d0d0a
joshhsoj1902 <joshhsoj1902@gmail.com>
2015-08-21 16:41:39 -0400

    Added library file for working with handlebars templates on ther server side.

---
+++ components/feed/hbsLib.js
@@ -0,0 +1,48 @@
+(function () {
+
+	var expresshbs = require('express-handlebars');
+	var expressHandlebars = expresshbs.create({
+		// Specify helpers which are only registered on this instance. 
+		helpers: {
+			foo: function () { return 'FOO!'; },
+			bar: function () { return 'BAR!'; }
+		}
+				});
+	var fs = require('fs');
+
+	var hbsLocal = {
+		getTemplate: function (templateName) {
+
+			var templateLocation;
+			var template;
+
+			switch (templateName) {
+				case "collapse":
+					templateLocation = __dirname + "/templates/collapse.html";
+					break;
+
+				default:
+					break;
+			}
+			
+			//TODO: put templates into an array and reuse them instead of reloading them 
+			if (templateLocation !== "undefined") {
+
+				var file = fs.readFileSync(templateLocation, "utf8");
+				template = expressHandlebars.handlebars.compile(file);
+			}
+
+			return template;
+		}
+
+	};
+	module.exports = {
+		buildHandlebarsHTML: function (templateName, context) {
+
+			var template = hbsLocal.getTemplate(templateName);
+			var html = template(context);
+
+			return html;
+		}
+	};
+} ());
